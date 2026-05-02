4a28f5162a243906a3ebec0bf90e0778668ad296
Sokhna Mame Seye Mbacké Mboup <mameseye733@gmail.com>
2026-05-02 16:53:47 +0000

    Lab 2 : exercices couleur, StatReseau et lignes supplémentaires

--- src/LigneBus.js
+++ src/LigneBus.js
@@ -1,9 +1,13 @@
 import './LigneBus.css';
 
-function LigneBus({ numero, depart, arrivee, arrets }) {
+function LigneBus({ numero, depart, arrivee, arrets, couleur }) {
+  const badgeColor = couleur || '#0a6e31';
+
   return (
     <div className="ligne-bus">
-      <div className="ligne-numero">{numero}</div>
+      <div className="ligne-numero" style={{ backgroundColor: badgeColor }}>
+        {numero}
+      </div>
       <div className="ligne-info">
         <span className="ligne-trajet">
           {depart} &rarr; {arrivee}
