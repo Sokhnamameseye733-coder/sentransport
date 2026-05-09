6b84c44c2fe6e5b3c26ffbe3e695edf47540308f
Sokhna Mame Seye Mbacké Mboup <mameseye733@gmail.com>
2026-05-09 12:06:43 +0000

    Lab 3 : etat, recherche et selection de lignes

--- src/LigneBus.js
+++ src/LigneBus.js
@@ -1,17 +1,18 @@
 import './LigneBus.css';
 
-function LigneBus({ numero, depart, arrivee, arrets, couleur }) {
-  const badgeColor = couleur || '#0a6e31';
+function LigneBus({ numero, depart, arrivee, arrets, couleur, estSelectionnee, onClick }) {
+  const badgeColor = estSelectionnee ? '#e67e22' : (couleur || '#0a6e31');
 
   return (
-    <div className="ligne-bus">
+    <div
+      className={`ligne-bus ${estSelectionnee ? 'ligne-bus-active' : ''}`}
+      onClick={onClick}
+    >
       <div className="ligne-numero" style={{ backgroundColor: badgeColor }}>
         {numero}
       </div>
       <div className="ligne-info">
-        <span className="ligne-trajet">
-          {depart} &rarr; {arrivee}
-        </span>
+        <span className="ligne-trajet">{depart} → {arrivee}</span>
         <span className="ligne-arrets">{arrets} arrêts</span>
       </div>
     </div>
